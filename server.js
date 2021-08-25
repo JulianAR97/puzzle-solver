1d0c597a101c99c428bb15fba9dc0eca8ab766fe
Julian <julianrosenthal@yahoo.com>
2021-08-24 22:29:01 -0700

    check route working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const Sudoku = require('./Sudoku.js')
+const Helpers = require('./Helpers.js')
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,18 @@ app.post('/solve', (req, res) => {
   }
 })
 
+app.post('/check', (req, res) => {
+  const puzzle = req.body;
+  let matrix = Sudoku.convertToMatrix(puzzle);
+  let s = new Sudoku(matrix);
+  s.solve()
+
+  if (s.solution && Helpers.arrayDiff(puzzle, s.solution.flat()).length === 0) {
+    res.json({errors: ['Solution is Correct']})
+  } else {
+    res.json({errors: ['Invalid Solution']})
+  }
+})
 
 const port = process.env.PORT || 8001
 app.listen(port, () => console.log(`Listening on port ${port}`))
